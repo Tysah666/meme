b968049e1acc64367893e23c2fd9f55ebf2c206d
Tysah666 <159538808+Tysah666@users.noreply.github.com>
2024-02-16 06:13:09 -0800

    kuns

---
+++ htdocs/growtopia/server_data.php
@@ -0,0 +1,11 @@
+server|38.46.217.75
+port|51520
+type|1
+#maint|Server currently change hosting, please join whatsapp to get the latest host.
+
+beta_server|142.93.45.168
+beta_port|17092
+
+beta_type|1
+meta|localhost
+RTENDMARKERBS1001
